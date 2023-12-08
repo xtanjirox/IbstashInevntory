98e40db0ed552f52d8b3093a14f34df4f789c0f4
Mehdi <xtanjirokamado93@gmail.com>
2023-12-09 00:18:25 +0100

    generate code

--- ibstash_inventory/apps/core/management/commands/clean_code_qr.py
+++ ibstash_inventory/apps/core/management/commands/clean_code_qr.py
@@ -6,4 +6,4 @@ from core import models
 class Command(BaseCommand):
     def handle(self, *args, **options):
         old_bar_codes = models.BarCode.objects.filter()
-        os.remove("/home/mehdi/Documents/projects/IbstashInevntory/ibstash_inventory/code_qr/96968db74111b46f09552d11f28e206e.png")
+        os.remove("/home/mehdi/Documents/projects/IbstashInevntory/ibstsh_inventory/code_qr/96968db74111b46f09552d11f28e206e.png")
